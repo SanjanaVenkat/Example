34a1ef3853fa01c30b5193cec5df7806b121fbec
U-BSD\349973 <349973@SHS-10L74C3F1.beaverton.k12.or.us>
2018-08-31 09:11:31 -0700

    first commit

---
+++ example.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int input = 0;
+  cin >> input;
+  if (input == 3)
+    { 
+    cout << "You put in a 3!" << endl;
+    input++;
+    }
+  else if (input == 4)
+    {
+      cout << "You put in a 4!" << endl;
+    }
+  else
+    {
+      cout << "You entered something else!" << endl;
+    } 
+  cout << input << endl;
+  return 0;
+}
